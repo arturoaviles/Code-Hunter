43af71e5e212ae2412867830ad68830b0e6c680e
Arturo Aviles <100636976@oncampus.local>
2016-12-09 23:45:44 -0500

    Adds File Detection in the same directory as the GO file

--- src/main/codehunter.go
+++ src/main/codehunter.go
@@ -1,13 +1,21 @@
 // Author: Arturo Aviles Castellanos
-// Date: 18 Nov 2016
+// Creation Date: 18 Nov 2016
 
 package main
 
 import (
     "fmt"
     //"path/filepath"
+
+    "io/ioutil"
 )
 
 func main(){
     fmt.Println("Hello World!")
+
+    files, _ := ioutil.ReadDir("./")
+    for _, f := range files {
+            fmt.Println(f.Name())
+    }
+
 }
