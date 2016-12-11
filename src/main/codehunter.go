3e3595b7616cd08e1269f2298099ebd9716c21b2
Arturo Aviles <100636976@oncampus.local>
2016-12-11 14:56:13 -0500

    Add Split File Extension from Path

--- src/main/codehunter.go
+++ src/main/codehunter.go
@@ -8,7 +8,7 @@ import (
     "os"
     "flag"
     "path/filepath"
-
+    "strings"
     //"io/ioutil"
 )
 
@@ -20,14 +20,27 @@ func main(){
     var testDirectory string = "C:\\Users\\100636976\\Desktop\\test"
     fmt.Println(testDirectory)
 
+
     flag.Parse()
     err := filepath.Walk(testDirectory, visit)
     fmt.Printf("filepath.Walk() returned %v\n", err)
-
-
 }
 
 func visit(path string, f os.FileInfo, err error) error {
-    fmt.Printf( "%s\n", path)
+    //fmt.Printf("%s\n", path)
+    getFileExtension(path)
+    return nil
+}
+
+func getFileExtension(path string) error {
+    var splitExtensionFromPath = strings.SplitAfterN(path, ".", 2)
+    //var extension string = splitExtensionFromPath[1]
+    //var extension string = "hello"
+    if len(splitExtensionFromPath) > 1 { // File
+        fmt.Println(splitExtensionFromPath[1])
+    } else { // Directory
+        fmt.Println("Directory: %s", path)
+    }
+
     return nil
 }
